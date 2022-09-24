9f8e099940391da8c57ba0a8a30fc3ae3ae81944
tWilltp <williamjenkinsfeb@gmail.com>
2022-09-24 16:17:35 +0000

    add basic js structure

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+let disk1 = size[0]
+
+let disk2 = size[1]
+
+let disk3 = size[2]
+
+function runGame() {
+
+}
+
+function selectDisk() {
+
+}
+
+function selectPole() {
+
+}
+
+function checkDiskSize() {
+    if disk > disk
+}
+
+function assignDisk() {
+
+}
+
+function incrementMoves() {
+
+}
+
+
